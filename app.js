ea8121837be09405349cf8b5248fdab5b541298f
tomcatling <tomcatling@gmail.com>
2019-10-19 20:31:59 +0100

    Get caller identity as a prefix.

--- app.js
+++ app.js
@@ -17,94 +17,121 @@ function getReadableFileSizeString(fileSizeInBytes) {
     return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
 }
 
-function uuidv4() {
-  return 'xxxxxxxxxxxxxxxxxxxx-'.replace(/[x]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
 
-function fixFilename(filename) {
+function fixFilename(username, filename) {
   var cleanfilename = filename.replace(/[^a-zA-Z0-9\.\-\_]+/g,"");
-  var trimmed = cleanfilename.slice(cleanfilename.length - 30)
-  return uuidv4() + trimmed
+  return username + '/' + cleanfilename
 }
 
-function Upload() {
+function validateInputs() {
   var files = document.getElementById("objectupload").files;
   if (!files.length) {
-    return alert("Please choose a file to upload first.");
+    return "Please choose a file to upload first.";
   }
   
   var file = files[0];
 
   if (file.size/1024/1024/1024 > 200) {
-    return alert("This file is too large. Individual submissions must be < 200GB.");
+    return "This file is too large. Individual submissions must be < 200GB.";
   }
-
-
-  var objectKey = fixFilename(file.name);
 
   var keyid = document.getElementById("keyid").value;
   var keysecret = document.getElementById("keysecret").value;
   var checked = document.getElementById("acceptanceCheck").checked;
 
   if (keyid === "" || keysecret === "") {
-    return alert("Please enter credentials first.");
+    return "Please enter credentials first.";
   }
 
   if (!checked) {
-    return alert("Please confirm that the data being submitted conforms to our guidelines.");
+    return "Please confirm that the data being submitted conforms to our guidelines.";
   }
 
-  var s3 = new AWS.S3({
-    apiVersion: "2006-03-01",
-    useAccelerateEndpoint: true,
-    clientSideMonitoring: true,
-    logger: console,
-    computeChecksums: true,
-    sslEnabled: true,
-    accessKeyId: keyid,
-    secretAccessKey: keysecret
-  });
+}
 
-  $('#progress .progress-bar').css('width',"0px");
-  $('#progress .progress-number').text("");
-  document.getElementById("addobject").disabled = true;
+function Upload() {
 
-  // Use S3 ManagedUpload class as it supports multipart uploads
-  s3.upload(
-    params = {
-      Bucket: bucketName,
-      Key: objectKey,
-      Body: file,
-      ACL: "private",
-      ServerSideEncryption: 'AES256',
-	},
-	options = {
-    partSize: 25 * 1024 * 1024, 
-    queueSize: 6
-  },
-    (err, data) => {
-      if (err){
-        $(".progress-number").html('<font color="red">'+err+'</font>');
-        console.log(err);
-      }
-      else {
-        $(".progress-number").html('Upload complete. <br> Your submission ID is: <font color="red">' + objectKey +'</font>')
-        console.log('Done.');
-      }
-      document.getElementById("addobject").disabled = false;
+  var msg = validateInputs()
+  if (msg) {return alert(msg);}
+
+  var file = document.getElementById("objectupload").files[0];
+  var keyid = document.getElementById("keyid").value;
+  var keysecret = document.getElementById("keysecret").value;
+
+  var sts = new AWS.STS(
+    options = {
+      sslEnabled: true,
+      clientSideMonitoring: true,
+      logger: console,
+      accessKeyId: keyid,
+      secretAccessKey: keysecret
     }
-  ).on('httpUploadProgress', function(progress) {
-      var progress = (progress.loaded * 100) / progress.total;
-      var progressInt = parseInt(progress)
-      console.log("Uploaded :: " + progress +'%');
-      $('#progress .progress-bar').css(
-            'width',
-            progressInt + '%'
-      );
-      $(".progress-number").html(getReadableFileSizeString(file.size * progress / 100)+" / "+getReadableFileSizeString(file.size));
-  });
+  );
+
+  var promise = sts.getCallerIdentity().promise();
+
+  // handle promise's fulfilled/rejected states
+  promise.then(
+    function(data) {
+      var username = data.Arn.split('/').slice(-1)[0]
+      var objectKey = fixFilename(username, file.name);
+
+      var s3 = new AWS.S3({
+        apiVersion: "2006-03-01",
+        useAccelerateEndpoint: true,
+        clientSideMonitoring: true,
+        logger: console,
+        computeChecksums: true,
+        sslEnabled: true,
+        accessKeyId: keyid,
+        secretAccessKey: keysecret
+      });
+
+      $('#progress .progress-bar').css('width',"0px");
+      $('#progress .progress-number').text("");
+      document.getElementById("addobject").disabled = true;
+
+      // Use S3 ManagedUpload class as it supports multipart uploads
+      s3.upload(
+      params = {
+        Bucket: bucketName,
+        Key: objectKey,
+        Body: file,
+        ACL: "private",
+        ServerSideEncryption: 'AES256',
+      },
+      options = {
+        partSize: 25 * 1024 * 1024, 
+        queueSize: 6
+      },
+        (err, data) => {
+          if (err){
+            $(".progress-number").html('<font color="red">'+err+'</font>');
+            console.log(err);
+          }
+          else {
+            $(".progress-number").html('Upload complete. <br> Your submission ID is: <font color="red">' + objectKey +'</font>')
+            console.log('Done.');
+          }
+          document.getElementById("addobject").disabled = false;
+        }
+      ).on('httpUploadProgress', function(progress) {
+          var progress = (progress.loaded * 100) / progress.total;
+          var progressInt = parseInt(progress)
+          console.log("Uploaded :: " + progress +'%');
+          $('#progress .progress-bar').css(
+                'width',
+                progressInt + '%'
+          );
+          $(".progress-number").html(getReadableFileSizeString(file.size * progress / 100)+" / "+getReadableFileSizeString(file.size));
+      });
+
+
+    },
+    function(error) {
+      return alert("Cannot validate credentials.");
+    }
+  );
+
   
 }
